d48f890eec2eab26036f00d948d691c00df58c22
Joseph Donofry <joedonofry@gmail.com>
2023-10-19 20:31:04 -0400

    Initial firefish openapi->swift build
    This is very 'raw' and not end-client friendly yet

--- Package.swift
+++ Package.swift
@@ -5,22 +5,46 @@ import PackageDescription
 
 let package = Package(
     name: "libswiftfish",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "libswiftfish",
             targets: ["libswiftfish"]),
+        .library(
+            name: "libswiftfishDynamic",
+            type: .dynamic,
+            targets: ["libswiftfish"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-generator", .upToNextMinor(from: "0.3.1")),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", .upToNextMinor(from: "0.3.3")),
+        .package(url: "https://github.com/apple/swift-openapi-urlsession", .upToNextMinor(from: "0.3.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "libswiftfish",
-            dependencies: []),
+            dependencies: [
+                .product(
+                    name: "OpenAPIRuntime",
+                    package: "swift-openapi-runtime"
+                ),
+                .product(
+                    name: "OpenAPIURLSession",
+                    package: "swift-openapi-urlsession"
+                ),
+            ],
+            path: "Sources",
+            plugins: [
+                .plugin(
+                    name: "OpenAPIGenerator",
+                    package: "swift-openapi-generator"
+                )
+            ]),
         .testTarget(
             name: "libswiftfishTests",
             dependencies: ["libswiftfish"]),
--- Sources/libswiftfish/libswiftfish.swift
+++ Sources/libswiftfish/libswiftfish.swift
@@ -1,6 +1,63 @@
-public struct libswiftfish {
-    public private(set) var text = "Hello, World!"
+import struct Foundation.URL
+import Foundation
 
-    public init() {
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+public struct libswiftfish {
+    public init() {}
+
+    struct ExtendedISO8601DateTranscoder: DateTranscoder {
+        func encode(_ date: Date) throws -> String {
+            ISO8601DateFormatter().string(from: date)
+        }
+
+        func decode(_ string: String) throws -> Date {
+            let extendedFormatter = ISO8601DateFormatter()
+            extendedFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+            let dateFormatters = [ISO8601DateFormatter(), extendedFormatter]
+            for formatter in dateFormatters {
+                if let result = formatter.date(from: string) {
+                    return result
+                }
+            }
+            throw DecodingError.dataCorrupted(.init(
+                codingPath: [],
+                debugDescription: "Date string does not match any of the expected formats"))
+        }
+    }
+
+    
+    public func getGreeting(name: String?) async throws -> String? {
+        let client = Client(
+            serverURL: URL(string: "https://nheko.io/api")!,
+            configuration: .init(dateTranscoder: ExtendedISO8601DateTranscoder()),
+            transport: URLSessionTransport()
+        )
+
+        //let input = Operations.notes_timeline.Input(
+        //    body: Operations.notes_timeline.Input.Body.json(Operations.notes_timeline.Input.Body.jsonPayload(limit: 10, sinceDate: 0, withReplies: true)))
+        let response = try await client.notes_local_hyphen_timeline(Operations.notes_local_hyphen_timeline.Input(body: Operations.notes_local_hyphen_timeline.Input.Body.json(Operations.notes_local_hyphen_timeline.Input.Body.jsonPayload(withFiles: false, excludeNsfw: true, sinceDate: 0, withReplies: true))))
+        switch response {
+        case .ok(let okResponse):
+            switch okResponse.body {
+            case .json(let greeting):
+                //return greeting.first!.text
+                return try greeting.first!.user.name
+            }
+        case .undocumented(statusCode: let statusCode, _):
+            return "🙉 \(statusCode)"
+        case .unauthorized(_):
+            return "unauthorized"
+        case .badRequest(_):
+            return "bad request"
+        case .forbidden(_):
+            return "4bidden"
+        case .internalServerError(_):
+            return "500"
+        case .code418(_):
+            return "🫖"
+
+        }
     }
 }
